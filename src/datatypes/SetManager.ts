5636862e0ea013505799f7455f8bd5577af99731
Patrick Scheich <scheichpatrick@gmail.com>
2023-10-27 22:50:31 +0200

    fix: Setmanager now calculates

--- src/datatypes/SetManager.ts
+++ src/datatypes/SetManager.ts
@@ -1,9 +1,15 @@
 import { store } from "@/stores/IonicStorage";
 import { useDatabaseStore } from "@/stores/databaseStore";
+/**
+ * SetManager class for managing sets and reps for exercises.
+ */
 export class SetManager {
   private m_Reps: number;
   private db;
 
+  /**
+   * Constructor for SetManager class.
+   */
   constructor() {
     this.m_Reps = 8;
     store.get("GoalAnatomy").then((value) => {
@@ -32,14 +38,31 @@ export class SetManager {
       : console.error("database is null");
   }
 
+  /**
+   * Sets the number of reps.
+   * @param reps - The number of reps.
+   */
   public setReps(reps: number): void {
     this.m_Reps = reps;
   }
 
+  /**
+   * Gets the number of reps.
+   * @returns The number of reps.
+   */
   public getReps(): number {
     return this.m_Reps;
   }
 
+  /**
+   * Calculates the 1RM for an exercise.
+   * @param exercise - The exercise to calculate the 1RM for.
+   * @param day - The day to calculate the 1RM for.
+   * @param days - The number of days to look back for the exercise.
+   * @param formula - The formula to use for the calculation.
+   * @param latest - Whether to use the latest workout for the calculation.
+   * @returns The calculated 1RM.
+   */
   public async get1RM(
     exercise: string,
     day: string,
@@ -54,6 +77,13 @@ export class SetManager {
     return Math.round(oneRM * 10) / 10;
   }
 
+  /**
+   * Calculates the 1RM from weight and reps.
+   * @param weight - The weight used for the exercise.
+   * @param reps - The number of reps.
+   * @param formula - The formula to use for the calculation.
+   * @returns The calculated 1RM.
+   */
   public calculate1RMFromWeightAndReps(
     weight: number,
     reps: number,
@@ -64,9 +94,12 @@ export class SetManager {
         Math.round(
           ((this.calculate1RMBoydEppley(weight, reps) +
             this.calculate1RMBrzycki(weight, reps) +
-            this.calculate1RMLander(weight, reps)) /
-            3) *
-            10
+            this.calculate1RMLander(weight, reps) +
+            this.calculate1RMLombardi(weight, reps) +
+            this.calculate1RMOConner(weight, reps) +
+            this.calculate1RMMayhew(weight, reps)) *
+            10) /
+            5
         ) / 10
       );
     } else if (formula === "eppley") {
@@ -75,9 +108,22 @@ export class SetManager {
       return Math.round(this.calculate1RMBrzycki(weight, reps) * 10) / 10;
     } else if (formula === "lander") {
       return Math.round(this.calculate1RMLander(weight, reps) * 10) / 10;
+    } else if (formula === "oconner") {
+      return Math.round(this.calculate1RMOConner(weight, reps) * 10) / 10;
+    } else if (formula === "lombardi") {
+      return Math.round(this.calculate1RMLombardi(weight, reps) * 10) / 10;
+    } else if (formula === "mayhew") {
+      return Math.round(this.calculate1RMMayhew(weight, reps) * 10) / 10;
     }
   }
 
+  /**
+   * Calculates the NRM from weight and reps.
+   * @param weight - The weight used for the exercise.
+   * @param reps - The number of reps.
+   * @param formula - The formula to use for the calculation.
+   * @returns The calculated NRM.
+   */
   public calculateNRMFromWeightAndReps(
     weight: number,
     reps: number,
@@ -88,9 +134,12 @@ export class SetManager {
         Math.round(
           ((this.calculateNRMBoydEppley(weight, reps) +
             this.calculateNRMBrzycki(weight, reps) +
-            this.calculateNRMLander(weight, reps)) *
+            this.calculateNRMLander(weight, reps) +
+            this.calculateNRMOConner(weight, reps) +
+            this.calculateNRMLombardi(weight, reps) +
+            this.calculateNRMMayhew(weight, reps)) *
             10) /
-            3
+            5
         ) / 10
       );
     } else if (formula === "eppley") {
@@ -99,9 +148,24 @@ export class SetManager {
       return Math.round(this.calculateNRMBrzycki(weight, reps) * 10) / 10;
     } else if (formula === "lander") {
       return Math.round(this.calculateNRMLander(weight, reps) * 10) / 10;
+    } else if (formula === "oconner") {
+      return Math.round(this.calculateNRMOConner(weight, reps) * 10) / 10;
+    } else if (formula === "lombardi") {
+      return Math.round(this.calculateNRMLombardi(weight, reps) * 10) / 10;
+    } else if (formula === "mayhew") {
+      return Math.round(this.calculateNRMMayhew(weight, reps) * 10) / 10;
     }
   }
 
+  /**
+   * Calculates the NRM for an exercise.
+   * @param exercise - The exercise to calculate the NRM for.
+   * @param day - The day to calculate the NRM for.
+   * @param days - The number of days to look back for the exercise.
+   * @param formula - The formula to use for the calculation.
+   * @param latest - Whether to use the latest workout for the calculation.
+   * @returns The calculated NRM.
+   */
   public async getNRM(
     exercise: string,
     day: string,
@@ -117,6 +181,9 @@ export class SetManager {
     return Math.round(NRM * 10) / 10;
   }
 
+  /**
+   * Tests all algorithms.
+   */
   public test(): void {
     // test all algorithms
     const weight = 100;
@@ -135,6 +202,14 @@ export class SetManager {
     );
   }
 
+  /**
+   * Retrieves the reps and weight values for a given exercise and time period from the database.
+   * @param exercise - The name of the exercise to retrieve values for.
+   * @param day - The date to start retrieving values from (in YYYY-MM-DD format).
+   * @param days - The number of days to retrieve values for.
+   * @param latest - Whether to retrieve values for the latest workout only (defaults to false).
+   * @returns An array of objects containing the reps and weight values for the given exercise and time period.
+   */
   private async getValuesFromDB(
     exercise: string,
     day: string,
@@ -164,6 +239,13 @@ export class SetManager {
     return result.values;
   }
 
+  /**
+   * Calculates the 1RM of all values of result (array of WorkoutExercise; {weight: number, reps: number}})
+   * and returns the average of all 1RM's
+   * @param result - Array of WorkoutExercise objects containing weight and reps
+   * @param formula - The formula to use for calculating the 1RM. Can be one of "all", "optimal", "eppley", "brzycki", "lander", "oconner", "lombardi", or "mayhew"
+   * @returns The average of all 1RM's
+   */
   private calculate1RM(result: any, formula: string): number {
     // Calculate the 1RM of all values of result (array of WorkoutExercise; {weight: number, reps: number}})
     // and return the average of all 1RM's
@@ -222,6 +304,13 @@ export class SetManager {
     return averageOneRM;
   }
 
+  /**
+   * Calculates the NRM of all values of result (array of WorkoutExercise; {weight: number, reps: number}})
+   * and returns the average of all NRM's
+   * @param result - An array of WorkoutExercise objects containing weight and reps properties
+   * @param formula - A string representing the formula to use for NRM calculation
+   * @returns The average NRM of all values in the result array
+   */
   private calculateNRM(result: any, formula: string): number {
     // Calculate the NRM of all values of result (array of WorkoutExercise; {weight: number, reps: number}})
     // and return the average of all NRM's
@@ -266,6 +355,13 @@ export class SetManager {
   }
 
   // Formula after https://www.marathonfitness.de/maximalkraft-rechner/#:~:text=1RM%20%3D%20Gewicht%20%2B%20Gewicht%20×%20Wiederholungen,kraftorientiert%20im%20unteren%20Wiederholungsbereich%20trainieren.
+  /**
+   * Calculates the one-rep max using the Boyd Epley formula.
+   * @param weight The weight lifted.
+   * @param repetitions The number of repetitions performed.
+   * @returns The calculated one-rep max.
+   * @throws An error if the number of repetitions is less than or equal to 0.
+   */
   private calculate1RMBoydEppley(weight: number, repetitions: number): number {
     if (repetitions <= 0) {
       throw new Error("The number of repetitions must be greater than 0.");
@@ -280,6 +376,13 @@ export class SetManager {
   }
 
   // Formula after https://www.marathonfitness.de/maximalkraft-rechner/#:~:text=1RM%20%3D%20Gewicht%20%2B%20Gewicht%20×%20Wiederholungen,kraftorientiert%20im%20unteren%20Wiederholungsbereich%20trainieren.
+  /**
+   * Calculates the one-rep max (1RM) using the Brzycki formula.
+   * @param weight The weight lifted for the given number of repetitions.
+   * @param repetitions The number of repetitions performed.
+   * @returns The calculated 1RM.
+   * @throws An error if the number of repetitions is less than or equal to 0.
+   */
   private calculate1RMBrzycki(weight: number, repetitions: number): number {
     if (repetitions <= 0) {
       throw new Error("The number of repetitions must be greater than 0.");
@@ -290,6 +393,13 @@ export class SetManager {
   }
 
   // Formula after https://www.marathonfitness.de/maximalkraft-rechner/#:~:text=1RM%20%3D%20Gewicht%20%2B%20Gewicht%20×%20Wiederholungen,kraftorientiert%20im%20unteren%20Wiederholungsbereich%20trainieren.
+  /**
+   * Calculates the one-rep max (1RM) for the lander exercise based on the given weight and number of repetitions.
+   * @param weight The weight used for the exercise.
+   * @param repetitions The number of repetitions performed.
+   * @returns The calculated 1RM for the lander exercise.
+   * @throws An error if the number of repetitions is less than or equal to 0.
+   */
   private calculate1RMLander(weight: number, repetitions: number): number {
     if (repetitions <= 0) {
       throw new Error("The number of repetitions must be greater than 0.");
@@ -299,15 +409,29 @@ export class SetManager {
     return oneRM;
   }
 
+  /**
+   * Calculates the one-rep max (1RM) using the Conner formula.
+   * @param weight - The weight lifted.
+   * @param repetitions - The number of repetitions performed.
+   * @returns The calculated 1RM.
+   * @throws An error if the number of repetitions is less than or equal to 0.
+   */
   private calculate1RMOConner(weight: number, repetitions: number): number {
     if (repetitions <= 0) {
       throw new Error("The number of repetitions must be greater than 0.");
     }
 
-    const oneRM = weight * (1 + 0.25 * repetitions);
+    const oneRM = weight * (1 + 0.025 * repetitions);
     return oneRM;
   }
 
+  /**
+   * Calculates the one-rep max (1RM) using the Lombardi formula.
+   * @param weight The weight lifted.
+   * @param repetitions The number of repetitions performed.
+   * @returns The calculated 1RM.
+   * @throws An error if the number of repetitions is less than or equal to 0.
+   */
   private calculate1RMLombardi(weight: number, repetitions: number): number {
     if (repetitions <= 0) {
       throw new Error("The number of repetitions must be greater than 0.");
@@ -317,6 +441,13 @@ export class SetManager {
     return oneRM;
   }
 
+  /**
+   * Calculates the one-rep max (1RM) using the Mayhew formula.
+   * @param weight The weight lifted in kilograms.
+   * @param repetitions The number of repetitions performed.
+   * @returns The calculated 1RM.
+   * @throws An error if the number of repetitions is less than or equal to 0.
+   */
   private calculate1RMMayhew(weight: number, repetitions: number): number {
     if (repetitions <= 0) {
       throw new Error("The number of repetitions must be greater than 0.");
@@ -327,6 +458,12 @@ export class SetManager {
     return oneRM;
   }
 
+  /**
+   * Calculates the normalized relative muscularity (NRM) using the Boyd Eppley formula.
+   * @param weight - The weight lifted.
+   * @param repetitions - The number of repetitions performed.
+   * @returns The NRM calculated using the Boyd Eppley formula.
+   */
   private calculateNRMBoydEppley(weight: number, repetitions: number): number {
     // const w =
     //   this.calculate1RMBoydEppley(weight, repetitions) / (1 + this.m_Reps / 30);
@@ -336,6 +473,12 @@ export class SetManager {
     return w;
   }
 
+  /**
+   * Calculates the NRM (normalized repetition maximum) using the Brzycki formula.
+   * @param weight - The weight lifted for the given number of repetitions.
+   * @param repetitions - The number of repetitions performed.
+   * @returns The calculated NRM.
+   */
   private calculateNRMBrzycki(weight: number, repetitions: number): number {
     // const w =
     //   this.calculate1RMBrzycki(weight, repetitions) / ((37 - this.m_Reps) / 36);
@@ -344,6 +487,12 @@ export class SetManager {
     return w;
   }
 
+  /**
+   * Calculates the normalized weight for a given weight and number of repetitions.
+   * @param weight The weight used for the exercise.
+   * @param repetitions The number of repetitions performed.
+   * @returns The normalized weight.
+   */
   private calculateNRMLander(weight: number, repetitions: number): number {
     // const w =
     //   this.calculate1RMLander(weight, repetitions) /
@@ -355,15 +504,27 @@ export class SetManager {
     return w;
   }
 
+  /**
+   * Calculates the NRMO Conner value based on the given weight and repetitions.
+   * @param weight The weight used for the exercise.
+   * @param repetitions The number of repetitions performed.
+   * @returns The calculated NRMO Conner value.
+   */
   private calculateNRMOConner(weight: number, repetitions: number): number {
     // const w =
     //   this.calculate1RMOConner(weight, repetitions) /
     //   (1 + 0.25 * this.m_Reps);
     const w =
-      this.calculate1RMOConner(weight, repetitions) * (1 + 0.25 * this.m_Reps);
+      this.calculate1RMOConner(weight, repetitions) * (1 + 0.025 * this.m_Reps);
     return w;
   }
 
+  /**
+   * Calculates the normalized Lombardi value based on the given weight and repetitions.
+   * @param weight The weight used for the exercise.
+   * @param repetitions The number of repetitions performed for the exercise.
+   * @returns The calculated normalized Lombardi value.
+   */
   private calculateNRMLombardi(weight: number, repetitions: number): number {
     // const w =
     //   this.calculate1RMLombardi(weight, repetitions) /
@@ -374,6 +535,12 @@ export class SetManager {
     return w;
   }
 
+  /**
+   * Calculates the estimated 1 repetition maximum (1RM) using the Mayhew formula.
+   * @param weight - The weight lifted in kilograms.
+   * @param repetitions - The number of repetitions performed.
+   * @returns The estimated 1RM in kilograms.
+   */
   private calculateNRMMayhew(weight: number, repetitions: number): number {
     // const w =
     //   this.calculate1RMMayhew(weight, repetitions) /
